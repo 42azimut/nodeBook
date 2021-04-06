0fb961d2f7ca31a16d647142f31f78840efd88d2
42azimut <azimutuniverse@gmail.com>
2021-04-06 11:28:31 +0900

    config env

--- ch7/7-6/learn-sequelize/app.js
+++ ch7/7-6/learn-sequelize/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
+// const dotenv = require('dotenv');
+// dotenv.config();
 
 const { sequelize } = require('./models');
 // const indexRouter = require('./routes');
