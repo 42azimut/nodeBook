f17c1927d55d0d8494cc413b790136815758bd87
42azimut <azimutuniverse@gmail.com>
2021-05-15 00:18:34 +0900

    12.2 ws모듈로 웹 소켓 사용done

--- ch12/app.js
+++ ch12/app.js
@@ -14,16 +14,16 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'html');
 
-nunjucks.configure('view', {
+nunjucks.configure('views', {
   express: app,
   watch: true,
 })
 
 app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname), 'public'))
-app.use(express.join())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookkieParser(process.env.COOKIE_SECRET ))
+app.use(cookieParser(process.env.COOKIE_SECRET ))
 app.use(session({
   resave: false,
   saveUninitialized: false,
@@ -43,11 +43,11 @@ app.use((req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-  res.local.message = err.message,
-  res.local.error = process.env.NODE_ENV !== 'production' ? err : {};
-  res.status(err.status || 500)
-  res.render('error')
-})
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 const server = app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기 중');
